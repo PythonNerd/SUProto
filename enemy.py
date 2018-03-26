79a66cef576ee1e6b06bee4567688c364855cb9e
Odin <joseandroiddeveloper@gmail.com>
2018-03-25 23:32:51 -0400

    Revert "Added a comment on adding new features to enemy class"
    This reverts commit c2753760baa908f6187e43e64a25f780eb959da9.

--- enemy.py
+++ enemy.py
@@ -6,7 +6,6 @@ Rng will determine if they drop any assortment of items from
 the items file.
 
 Add methods
-Have to add how level will affect other stats of enemy
 '''
 class Enemy:
     def __init__(self, name, health, damage, rng, evasion, accuracy, level):
